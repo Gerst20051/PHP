dda5150245ef07fa186eb82af70fb8df38a507fc
Andrew Gerst <gerst20051@gmail.com>
2013-12-21 00:09:41 -0500

    css styles added to treeview

--- treeview/ajax.php
+++ treeview/ajax.php
@@ -1,3 +1,27 @@
 <?php
+require_once 'config.inc.php';
+require_once 'mysql.class.php';
 
+function print_json($data, $die = true){
+	header('Content-Type: application/json; charset=utf8');
+	print_r(json_encode($data));
+	if ($die === true) die();
+}
+
+$db = new MySQL;
+
+switch ($_SERVER['REQUEST_METHOD']) {
+case 'POST':
+
+	break;
+case 'GET':
+	$db->sfquery(array('SELECT * FROM `%s` WHERE timestamp > "%s"', MYSQL_TABLE, $_GET['timestamp']));
+	if ($db->numRows()) {
+		$result = $db->fetchParsedRows();
+		print_json(array('nodes'=>$result));
+	} else {
+		print_json(array('nodes'=>array()));
+	}
+	break;
+}
 ?>
--- treeview/treeview.js
+++ treeview/treeview.js
@@ -4,10 +4,131 @@
  *********************************************
  */
 
-var Node = function(){
+function addEvent(obj, type, fn){
+	if (obj.attachEvent) {
+		obj['e'+type+fn] = fn;
+		obj[type+fn] = function(){obj['e'+type+fn](window.event);}
+		obj.attachEvent('on'+type, obj[type+fn]);
+	} else {
+		obj.addEventListener(type, fn, false);
+	}
+}
 
+function removeEvent(obj, type, fn){
+	if (obj.detachEvent) {
+		obj.detachEvent('on'+type, obj[type+fn]);
+		obj[type+fn] = null;
+	} else {
+		obj.removeEventListener(type, fn, false);
+	}
+}
+
+var Node = function(){
+	this.id = 0;
+	this.text = "";
+	this.open = false;
+	this.parent = null;
+	this.children = [];
+};
+
+Node.prototype.open = function(){
+	this.open = !this.open;
+	// TODO: manipulate DOM
+	document.getElementById('node_' + this.id).getElementsByClassName('.children').hide();
+};
+
+Node.prototype.collapse = function(){
+	this.open = false;
+	// TODO: manipulate DOM
+};
+
+Node.prototype.rename = function(text){
+	this.text = text;
+	// TODO: manipulate DOM
+};
+
+Node.prototype.delete = function(){
+	this.parent.children = this.parent.children.slice(this.parent.children.indexOf(this), 1);
+	// TODO: manipulate DOM
+};
+
+Node.prototype.setParent = function(node){
+	this.parent = node;
+};
+
+Node.prototype.addFolder = function(){
+	var node = new Node();
+	node.setParent(this);
+	//this.children[] = node;
+};
+
+Node.prototype.addChild = function(){
+	var node = new Node();
+	node.setParent(this);
+	//this.children[] = node;
 };
 
 var TreeView = function(){
+	this.selector = "";
+	this.nodes = [];
+	this.tree = [];
+	this.output = [];
 
+	this.config = function(selector){
+		this.selector = selector;
+		this.attachHandlers();
+		return this;
+	};
+
+	this.run = function(){
+		//this.parseData();
+		this.printTree();
+	};
+};
+
+TreeView.prototype.setData = function(data){
+	this.nodes = data || [];
+	return this;
+};
+
+TreeView.prototype.updateData = function(data){
+	if (data) {
+		this.output.length = 0;
+		if (typeof data == "object") {
+			this.nodes = data;
+		} else if (typeof data == "string") {
+			this.nodes = JSON.parse(data);
+		}
+		this.run();
+	}
+};
+
+TreeView.prototype.getNodes = function(){
+	return this.nodes;
+};
+
+TreeView.prototype.printTree = function(){
+	document.getElementById(this.selector.slice(1)).innerHTML = this.output.join('');
+};
+
+TreeView.prototype.attachHandlers = function(){
+	var $div = $(this.selector), self = this;
+
+	$div.on('click', '.folder i', function(){
+		$(this).parent().toggleClass('open');
+	});
+
+	$div.on('contextmenu', 'li', function(){
+		/*
+		if ($(this).hasClass('folder')) {
+
+		} else {
+
+		}
+		*/
+
+
+
+		return false;
+	});
 };
